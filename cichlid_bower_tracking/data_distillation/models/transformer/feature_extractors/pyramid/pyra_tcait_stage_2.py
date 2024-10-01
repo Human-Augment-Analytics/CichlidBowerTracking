5ad5435ef6c746b315435580c2e2c2f4a15c2736
Charlie Clark <cr-clark@outlook.com>
2024-09-30 20:54:48 -0400

    Fixed bug in PyraTCAiT-stage2

--- cichlid_bower_tracking/data_distillation/models/transformer/feature_extractors/pyramid/pyra_tcait_stage_2.py
+++ cichlid_bower_tracking/data_distillation/models/transformer/feature_extractors/pyramid/pyra_tcait_stage_2.py
@@ -64,10 +64,10 @@ class PyraTCAiTStage2(nn.Module):
         self.positive_pos_encoder = PositionalEncoding(embed_dim=self.embed_dim, n_patches=self.patcher.get_num_patches(self.in_dim), add_one=False)
         self.negative_pos_encoder = PositionalEncoding(embed_dim=self.embed_dim, n_patches=self.patcher.get_num_patches(self.in_dim), add_one=False)
 
-        # self.transformer_stack = nn.Sequential(*[TransformerBlock(embed_dim=self.embed_dim, n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, use_sra=True, sr_ratio=self.sr_ratio) for _ in range(self.depth - 1)])
-        self.transformer_stack = nn.Sequential(*[TransformerBlock(embed_dim=self.embed_dim, n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, use_sra=True, sr_ratio=self.sr_ratio) for _ in range(self.depth)])
+        self.transformer_stack = nn.Sequential(*[TransformerBlock(embed_dim=self.embed_dim, n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, use_sra=True, sr_ratio=self.sr_ratio) for _ in range(self.depth - 1)])
+        # self.transformer_stack = nn.Sequential(*[TransformerBlock(embed_dim=self.embed_dim, n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, use_sra=True, sr_ratio=self.sr_ratio) for _ in range(self.depth)])
 
-        # self.tca_block = TCABlock(embed_dim=self.embed_dim, n_patches=self.patcher.get_num_patches(self.in_dim), n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, init_alpha=self.init_alpha, init_beta=self.init_beta, add_one=self.add_cls)
+        self.tca_block = TCABlock(embed_dim=self.embed_dim, n_patches=self.patcher.get_num_patches(self.in_dim), n_heads=self.num_heads, p_dropout=self.dropout, mlp_ratio=self.mlp_ratio, init_alpha=self.init_alpha, init_beta=self.init_beta, add_one=self.add_cls)
 
     def __str__(self) -> str:
         '''
