d3496dd15ea210746f18e292223c674b1af0bab2
Charlie Clark <cr-clark@outlook.com>
2024-10-28 19:41:18 -0400

    Fixed bug (again) in _initial_embed

--- cichlid_bower_tracking/data_distillation/data_distiller.py
+++ cichlid_bower_tracking/data_distillation/data_distiller.py
@@ -198,7 +198,7 @@ class DataDistiller:
 
             for path in subset['path'].to_list():
                 path = self.base_data_dir + '/' + path
-                img = read_image(path).float()
+                img = read_image(path).float().unsqueeze(0)
                 embed = self.pretr_model(img)[-1]
 
                 self.embeddings[unique_id][path] = embed.item().flatten().to_list()
