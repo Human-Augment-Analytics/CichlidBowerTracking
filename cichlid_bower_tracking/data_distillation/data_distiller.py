f2de35b9991978de7ee16efa5ef285a5b755fd45
Charlie Clark <cr-clark@outlook.com>
2024-10-31 11:38:05 -0400

    Fixed bug in _load_embeddings

--- cichlid_bower_tracking/data_distillation/data_distiller.py
+++ cichlid_bower_tracking/data_distillation/data_distiller.py
@@ -836,10 +836,10 @@ class DataDistiller:
             with open(embeddings_path, 'r') as file:
                 tmp = json.load(file)
 
-                assert epoch == tmp['epoch'], f'Error: Previous epoch ({epoch - 1}) doesn\'t match stored epoch ({tmp["epoch"]}).'
+                assert epoch == int(tmp['epoch']), f'Error: Previous epoch ({epoch - 1}) doesn\'t match stored epoch ({tmp["epoch"]}).'
                 
                 self.embeddings = tmp['store']
-                metric = tmp['metric']
+                metric = float(tmp['metric'])
 
                 del tmp
 
